1acbdbae0701687a7b02628030ceb92a08c452ac
chadcarnahan <chadcarnahan1@yahoo.com>
2021-08-18 17:37:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/date");
+const cors = require("cors");
 let port = process.env.PORT || 80;
 
 app.use(express.static("./public"));
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/", router);
