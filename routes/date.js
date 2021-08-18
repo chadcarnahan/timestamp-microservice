d42d5a4a2bea61368cdf9a18cd4a57858edf104c
chadcarnahan <chadcarnahan1@yahoo.com>
2021-08-18 18:15:51 -0500

    Update date.js

--- routes/date.js
+++ routes/date.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getTime, getCurrent } = require("../controllers/controller");
 
-router.route("/:date?").get(getTime);
+router.route("/:date").get(getTime);
 router.route("/").get(getCurrent);
 
 module.exports = router;
